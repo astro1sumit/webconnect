59db655851552a6586e121573ff9c9c199b3c9c2
astro1sumit <astro.sumit.usa@gmail.com>
2025-06-14 14:40:55 +0530

    Authentication setup-Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import {Button} from "@/components/ui/button";
-import Image from "next/image";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Sign in</Button>
+    <div className="m-10" >
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
     </div>
   );
 }
